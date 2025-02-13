6ccede9ed7069d76f2e1052034f150d66d0ae1ec
tofustream <177463621+tofustream@users.noreply.github.com>
2025-02-14 00:45:22 +0900

    ReleaseModeを追加した。

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tofustream/simple-auth-service/config"
 	"github.com/tofustream/simple-auth-service/routes"
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	config.LoadCORSConfig()
 	log.Println("Allowed Origins:", config.AllowOrigins)
 
